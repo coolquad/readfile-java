b0571b6923dbee5515a5dddee0f4b74dae5db4cf
snow <54613944+whiteSnow0213@users.noreply.github.com>
2020-09-16 15:49:15 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.*; 
-import java.util.function.Supplier; 
-import java.io.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 public class Main
 {
 	public static void main(String[] args) {
